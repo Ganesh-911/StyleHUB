2a5df452963089784f4f16e2570cbd3039517782
student BVCE <ganeshgundubogula911@gmail.com>
2026-06-22 19:59:25 +0530

    add authentication

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     console.log("Trying to connect...");
-    console.log(process.env.MONGODB_URI);
-
     const conn = await mongoose.connect(process.env.MONGODB_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
