27f87ed15a8c0c54c3976f5b98ab70f7e72e7383
student BVCE <ganeshgundubogula911@gmail.com>
2026-06-23 20:34:45 +0530

    Added CRUD operations

--- Backend/src/controllers/productController.js
+++ Backend/src/controllers/productController.js
@@ -81,4 +81,24 @@ export const updateProduct= async(req,res)=>{
         });
     }
 };
-
+export const deleteProduct= async(req,res)=>{
+    try{
+        const product= await Product.findByIdAndDelete(req.params.id);
+        if(!product){
+            return res.status(404).json({
+                success:false,
+                message:"Product not found",
+            });
+        }
+        res.status(200).json({
+            success:true,
+            message:"Product deleted successfully",
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+};
--- Backend/src/routes/productRoutes.js
+++ Backend/src/routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {createProduct,getProducts,getProductById,updateProduct} from "../controllers/productController.js";
+import {createProduct,getProducts,getProductById,updateProduct,deleteProduct} from "../controllers/productController.js";
 
 const router=express.Router();
 router.post("/",createProduct);
 router.get("/",getProducts);
 router.get("/:id",getProductById);
 router.put("/:id",updateProduct);
+router.delete("/:id",deleteProduct);
 export default router;
