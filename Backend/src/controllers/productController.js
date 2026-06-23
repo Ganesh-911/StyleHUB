147efa81e0aa4a157830219c0f7d28e1ded2ce30
student BVCE <ganeshgundubogula911@gmail.com>
2026-06-23 20:30:56 +0530

    Added Update products

--- Backend/src/controllers/productController.js
+++ Backend/src/controllers/productController.js
@@ -53,3 +53,32 @@ export const getProductById= async(req,res)=>{
         });
     }
 };
+export const updateProduct= async(req,res)=>{
+    try{
+        const product=await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new:true,
+                runValidators:true,
+            }
+        );
+        if(!product){
+            return res.status(404).json({
+                success:false,
+                message:"Product not found",
+            });
+        }
+        res.status(200).json({
+            success:true,
+            message:"Product updated successfully",
+            product,
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+};
+
--- Backend/src/routes/productRoutes.js
+++ Backend/src/routes/productRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {createProduct,getProducts,getProductById} from "../controllers/productController.js";
+import {createProduct,getProducts,getProductById,updateProduct} from "../controllers/productController.js";
 
 const router=express.Router();
 router.post("/",createProduct);
 router.get("/",getProducts);
 router.get("/:id",getProductById);
+router.put("/:id",updateProduct);
 export default router;
