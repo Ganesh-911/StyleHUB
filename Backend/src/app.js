ef0ed87c13327b7bef143c6f8dfb2465f64c68a6
student BVCE <ganeshgundubogula911@gmail.com>
2026-06-23 20:21:20 +0530

    Added product schema

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/products", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("StyleHub API Running...");
---
+++ Backend/src/controllers/productController.js
@@ -0,0 +1,34 @@
+import Product from "../models/product.js";
+
+export const createProduct = async (req, res) => {
+    try{
+        const product = await Product.create(req.body);
+        res.status(201).json({
+            success:true,
+            message:"Product created successfully",
+            product,
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+};
+export const getProducts= async(req,res)=>{
+    try{
+        const products=await Product.find();    
+    res.status(200).json({
+        success:true,
+        count:products.length,
+        products,
+    });
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+};
