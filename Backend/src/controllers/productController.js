325860462a0378bc71d0210098dfddf5c20e5614
student BVCE <ganeshgundubogula911@gmail.com>
2026-06-24 16:54:11 +0530

    Added Paging

--- Backend/src/controllers/productController.js
+++ Backend/src/controllers/productController.js
@@ -30,11 +30,15 @@ export const getProducts = async (req, res) => {
     if (req.query.category) {
       query.category = req.query.category;
     }
+    const page = Number(req.query.page) || 1;
+    const limit = 4;
 
-    const products = await Product.find(query);
+    const skip = (page - 1) * limit;
+    const products = await Product.find(query).limit(limit).skip(skip);
 
     res.status(200).json({
       success: true,
+      page,
       count: products.length,
       products,
     });
