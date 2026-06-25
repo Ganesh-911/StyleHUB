4ad48374af720d7b951cb4eb172f2ebbc4c30114
student BVCE <ganeshgundubogula911@gmail.com>
2026-06-25 20:42:59 +0530

    Added my Orders

--- Backend/src/controllers/orderController.js
+++ Backend/src/controllers/orderController.js
@@ -47,3 +47,21 @@ export const createOrder = async (req, res) => {
         });
     }
 }
+export const getMyOrders= async(req,res)=>{
+    try{
+        const orders=await Order.find({
+            user:req.user._id
+        });
+        res.status(200).json({
+            success:true,
+            count: orders.length,
+            orders
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message
+        });
+    }
+};
