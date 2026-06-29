e51a98718c2d91f420630154c855dc6b8535bfa4
student BVCE <ganeshgundubogula911@gmail.com>
2026-06-29 11:11:42 +0530

    Added Wishlist module

--- Backend/src/controllers/wishlistController.js
+++ Backend/src/controllers/wishlistController.js
@@ -40,3 +40,50 @@ export const addToWishlist = async (req, res) => {
         });
     }
 };
+export const getMyWishlist = async (req, res) => {
+    try {
+
+        const wishlist = await Wishlist.find({
+            user: req.user._id,
+        }).populate("product");
+
+        res.status(200).json({
+            success: true,
+            count: wishlist.length,
+            wishlist,
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+
+    }
+};
+export const removeFromWishlist=async(req,res)=>{
+    try{
+        const wishlistItem=await Wishlist.findOne({
+            user:req.user._id,
+            product:req.params.productId,
+        });
+        if(!wishlistItem){
+            return res.status(404).json({
+                success:false,
+                message:"Product not found in Wishlist",
+            });
+        }
+        await wishlistItem.deleteOne();
+        res.status(200).json({
+            success:true,
+            message:"Product removed from wishlist",
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            message:error.message,
+        });
+    }
+};
--- Backend/src/routes/wishlistRoutes.js
+++ Backend/src/routes/wishlistRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware.js";
-import { addToWishlist } from "../controllers/wishlistController.js";
+import { addToWishlist,getMyWishlist,removeFromWishlist } from "../controllers/wishlistController.js";
 
 const router = express.Router();
 
 router.post("/", protect, addToWishlist);
+router.get("/",protect,getMyWishlist);
+router.delete("/:productId",protect,removeFromWishlist);
 
 export default router;
