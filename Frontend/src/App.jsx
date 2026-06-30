72fab6d841d6e92e2677aba627b865bf791b12e4
student BVCE <ganeshgundubogula911@gmail.com>
2026-06-30 19:31:46 +0530

    Frontend setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 function App() {
   return (
+    
     <div className="min-h-screen flex items-center justify-center bg-slate-900">
       <h1 className="text-5xl font-bold text-white">
         StyleHub Frontend 🚀
