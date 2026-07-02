ed1d31ecc799dad67b2c2da7287752fe315b7c78
student BVCE <ganeshgundubogula911@gmail.com>
2026-07-02 22:38:07 +0530

    Frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
@@ -9,7 +10,12 @@ import paymentRoutes from "./routes/paymentRoutes.js";
 import dashboardRoutes from "./routes/dashboardRoutes.js";
 
 const app = express();
-
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/products", productRoutes);
